7438fbbf35b2a0da836d938fa283d40533e482ee
Fabriciano Carrasco <fabricianocarrasco@gmail.com>
2019-04-09 22:39:24 +0200

    Creación de los modelos

--- src/es/upm/dit/isst/grupo06/cita/model/Cita.java
+++ src/es/upm/dit/isst/grupo06/cita/model/Cita.java
@@ -1,12 +1,29 @@
 package es.upm.dit.isst.grupo06.cita.model;
 
 import java.io.Serializable;
+import java.sql.Date;
+import java.sql.Time;
 
 import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
 
 @Entity
 public class Cita implements Serializable {
 	private static final long serialVersionUID = 1L;
 	
-	// TODO: Atributos cita
+	@Id
+	private int id;
+	private Date fecha;
+	private Time hora;
+	@ManyToOne
+	private Medico medico;
+	@ManyToOne
+	private Paciente paciente;
+	private boolean confirmada;
+	private boolean urgencia;
+	@ManyToOne
+	private Consulta consulta;
+	
+	
 }
--- src/es/upm/dit/isst/grupo06/cita/model/Consulta.java
+++ src/es/upm/dit/isst/grupo06/cita/model/Consulta.java
@@ -3,10 +3,14 @@ package es.upm.dit.isst.grupo06.cita.model;
 import java.io.Serializable;
 
 import javax.persistence.Entity;
+import javax.persistence.Id;
 
 @Entity
 public class Consulta implements Serializable {
 	private static final long serialVersionUID = 1L;
 	
-	// TODO: Atributos consulta
+	@Id
+	private int numero;
+	private int planta;
+	private String departamento;
 }
