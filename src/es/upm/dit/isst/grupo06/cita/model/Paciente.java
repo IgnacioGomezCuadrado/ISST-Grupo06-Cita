34336f896f13e70b2489776b81094b106a88b488
marcosnotario09 <36533890+marcosnotario09@users.noreply.github.com>
2019-04-10 14:56:59 +0200

    Update Paciente.java

--- src/es/upm/dit/isst/grupo06/cita/model/Paciente.java
+++ src/es/upm/dit/isst/grupo06/cita/model/Paciente.java
@@ -20,4 +20,76 @@ public class Paciente implements Serializable {
 	private String sociedadMedica;
 	private String tarjetaSanitaria;
 	
+	
+	
+public Paciente() {
+	
+}
+
+public int getId() {
+	return id;
+}
+
+public String getNombre() {
+	return nombre;
+}
+
+public void setNombrel(String nombre) {
+	this.nombre = nombre;
+}
+
+public String getApellidos() {
+	return apellidos;
+}
+
+public void setApellidosl(String apellidos) {
+	this.apellidos = apellidos;
+}
+
+public String getDni() {
+	return dni;
+}
+
+public void setDni(String dni) {
+	this.dni = dni;
+}
+
+
+public String getEmail() {
+	return email;
+}
+
+public void setEmail(String email) {
+	this.email = email;
+}
+
+public int getTelefono() {
+	return telefono;
+}
+
+public void setTelefono(int telefono) {
+	this.telefono = telefono;
+}
+
+public String getSociedadMedica() {
+	return sociedadMedica;
+}
+
+public void setSociedadMedica(String sociedadMedica) {
+	this.sociedadMedica = sociedadMedica;
+}
+
+
+public String getTarjetaSanitaria() {
+	return tarjetaSanitaria;
+}
+
+public void setTarjetaSanitarial(String tarjetaSanitaria) {
+	this.tarjetaSanitaria = tarjetaSanitaria;
+}
+
+	
+	
+	
+	
 }
