fce77d15c94962bde5c0d62903b664db37436500
Daniel Criado <danicri96@hotmail.com>
2019-04-10 16:44:12 +0200

    Pedir cita servlet

---
+++ src/es/upm/dit/isst/grupo06/cita/servlets/PedirCitaServlet.java
@@ -0,0 +1,25 @@
+package es.upm.dit.isst.grupo06.cita.servlets;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+
+@WebServlet("/PedirCitaServlet")
+public class PedirCitaServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		response.getWriter().append("Served at: ").append(request.getContextPath());
+	}
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
