b16ab5ef2d39303dc8ab5034eab26278e1efc679
marcosnotario09 <36533890+marcosnotario09@users.noreply.github.com>
2019-04-15 17:44:51 +0200

    Update ResetPasswordServlet.java

--- src/es/upm/dit/isst/grupo06/cita/servlets/ResetPasswordServlet.java
+++ src/es/upm/dit/isst/grupo06/cita/servlets/ResetPasswordServlet.java
@@ -8,6 +8,9 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import es.upm.dit.isst.grupo06.cita.dao.PacienteDAO;
+import es.upm.dit.isst.grupo06.cita.dao.PacienteDAOImplementation;
+import es.upm.dit.isst.grupo06.cita.model.Paciente;
 
 @WebServlet ("/ResetPasswordServlet")
 public class ResetPasswordServlet extends HttpServlet {
@@ -21,8 +24,29 @@ public class ResetPasswordServlet extends HttpServlet {
         String passwordNuevaRep = req.getParameter("passwordNuevaRep");
 
         //Actualizar ddbb
+        
+        PacienteDAO dao = PacienteDAOImplementation.getInstance();
+        Paciente paciente = dao.read(dni);
+
+        if (dni.equals(paciente.getDni()) && passwordActual.equals(paciente.getPassword())){
+
+            if(passwordNueva.equals(passwordNuevaRep)) {
+                paciente.setPassword(passwordNueva);
+                dao.update(paciente);
+                System.out.println("ContraseÃ±a cambiada con Ã©xito");
+                resp.sendRedirect(req.getContextPath() + "/LoginView.jsp");
+                //Notificacion mediante correo
+            }else {
+             System.out.println("Las contraseÃ±as nuevas no coinciden");
+                resp.sendRedirect(req.getContextPath());
+            }
+        } else{
+            System.out.println("Usuario o contraseÃ±a incorrectos");
+            resp.sendRedirect(req.getContextPath());
+        }
+    }
+}
+
 
       
-}
 
-}
