b4724032a38e5770c37d75261a155b7dfaa4ea1c
Daniel Criado <danicri96@hotmail.com>
2019-05-05 19:31:08 +0200

    Creado ModificarCitaServlet

---
+++ src/es/upm/dit/isst/grupo06/cita/servlets/ModificarCitaServlet.java
@@ -0,0 +1,38 @@
+package es.upm.dit.isst.grupo06.cita.servlets;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import es.upm.dit.isst.grupo06.cita.dao.CitaDAO;
+import es.upm.dit.isst.grupo06.cita.dao.CitaDAOImplementation;
+import es.upm.dit.isst.grupo06.cita.model.Cita;
+
+import es.upm.dit.isst.grupo06.cita.model.Paciente;
+
+/**
+ * Servlet implementation class BorrarCitaServlet
+ */
+@WebServlet("/ModificarCitaServlet")
+public class ModificarCitaServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		String idCita = req.getParameter("cita");
+		
+		// Pedimos la cita en la base de datos
+		CitaDAO citdao = CitaDAOImplementation.getInstance();
+		Cita cita = citdao.read(Integer.parseInt(idCita));
+		
+		Paciente paciente = (Paciente) req.getSession().getAttribute("paciente");
+		
+		// Redirigimos al paciente a la pantalla inicial donde puede ver las citas
+		resp.sendRedirect(req.getContextPath() + "/PacienteServlet?id=" + paciente.getEmail());
+	}
+
+
+}
